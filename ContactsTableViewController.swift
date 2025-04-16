6b6b71d4640b6ddb1348a2d7fee73214c36da383
Avb2 <bringuel.alexander@gmail.com>
2025-04-15 20:41:46 -0400

    tableview works but tableviewcontroller ui wont

--- ContactsTableViewController.swift
+++ ContactsTableViewController.swift
@@ -6,16 +6,46 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ContactsTableViewController: UITableView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var contacts: [NSManagedObject] = []
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        loadDataFromDatabase()
     }
-    */
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+    
+    func loadDataFromDatabase() {
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        do {
+            contacts = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: UITableVieCell) {
+        let cell = tableView.dequeReusableCell(withIdentifier: "ContactsCell", for: indexPath)
+        
+        let contact = contacts[indexPath.row] as? Contact
+        
+        cell.textLabel?.text = contact?.city
+        
+        return cell
+    }
+    
     
     override func tableView(_ tableview:, TableView, cellForRowAt: IndexPath) -> UITableViewCell {
         let cell = tableview.dequeueReusableCell(withIdentifier: "ContactsCell", for: indexPath)
